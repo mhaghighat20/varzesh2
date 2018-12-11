31559a1b8c2253a63ea2c46dd7f20fd9911de3cf
Mohammad Hossein Mostmand <mostmand.mh@gmail.com>
2018-12-11 15:20:59 +0330

    Some small changes to fix navigationBar behavior

--- src/NavigationBar.js
+++ src/NavigationBar.js
@@ -15,11 +15,11 @@ export default class NavigationBar extends React.Component{
                     </div>
                     <ul className="nav navbar-nav">
                         <li><NavLink exact to="/" className="navbar-link">خانه</NavLink></li>
-                        <li><NavLink to="/news?id=1" className="navbar-link">خبر</NavLink></li>
-                        <li><NavLink to="/league" className="navbar-link">لیگ‌</NavLink></li>
-                        <li><NavLink to="/team" className="navbar-link">تیم</NavLink></li>
-                        <li><NavLink to="/player?id=1" className="navbar-link">بازیکن</NavLink></li>
-                        <li><NavLink to="/game" className="navbar-link">بازی</NavLink></li>
+                        <li><NavLink isActive={(_, { pathname }) => pathname.startsWith('/news')} to="/news?id=1" className="navbar-link">خبر</NavLink></li>
+                        <li><NavLink isActive={(_, { pathname }) => pathname.startsWith('/league')} to="/league" className="navbar-link">لیگ‌</NavLink></li>
+                        <li><NavLink isActive={(_, { pathname }) => pathname.startsWith('/team')} to="/team" className="navbar-link">تیم</NavLink></li>
+                        <li><NavLink isActive={(_, { pathname }) => pathname.startsWith('/player')} to="/player?id=1" className="navbar-link">بازیکن</NavLink></li>
+                        <li><NavLink isActive={(_, { pathname }) => pathname.startsWith('/game')} to="/game" className="navbar-link">بازی</NavLink></li>
                     </ul>
                 </div>
             </div>
