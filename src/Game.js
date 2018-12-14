c588c0a31e74b287e44132642be2918ac87f6204
Mohammad Haghighat <mhaghighat@rayana.ir>
2018-12-14 22:55:10 +0330

    اصلاح صفحه بازی

--- src/Game.js
+++ src/Game.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { GameResult, GamesFull } from "./SharedComponents/GameResult";
-import { EventItem, EventsFull } from "./GameComponents/Events";
+import { EventItem, EventsFull, DoubleTeamItem, DoubleTeam } from "./GameComponents/Events";
 
 export default class Game extends React.Component{
     render() {
@@ -14,17 +14,28 @@ export default class Game extends React.Component{
         events.push(<EventItem leftOrRight='0' player='صدری' act='کارت زرد' minute='80' />);
         let eventsFull = <EventsFull EventItems={events} leftTeam="پرسپولیس" rightTeam="پدیده" />;
 
+        let stats = [];
+        stats.push(<DoubleTeamItem leftString="2" middleString="تعداد گل" rightString="1" />)
+        stats.push(<DoubleTeamItem leftString="67%" middleString="درصد مالکیت توپ" rightString="33%" />)
+        stats.push(<DoubleTeamItem leftString="2" middleString="کرنر" rightString="3" />)
+        stats.push(<DoubleTeamItem leftString="3" middleString="خطا" rightString="1" />)
+        stats.push(<DoubleTeamItem leftString="3" middleString="موقعیت گل" rightString="4" />)
+        stats.push(<DoubleTeamItem leftString="2" middleString="کارت زرد" rightString="1" />)
+        stats.push(<DoubleTeamItem leftString="0" middleString="کارت قرمز" rightString="0" />)
+        stats.push(<DoubleTeamItem leftString="صدری" middleString="بهترین بازیکن زمین" rightString="" />)
+
 
         let games = [];
         games.push(<GameResult leftTeam='پرسپولیس' rightTeam='پدیده' leftGoals='1' rightGoals='0' date='1397/07/13' />);
         games.push(<GameResult leftTeam='پدیده' rightTeam='پرسپولیس' leftGoals='0' rightGoals='1' date='1397/07/08' />);
-        games.push(<GameResult leftTeam='پدیده' rightTeam='ذوب آهن' leftGoals='1' rightGoals='0' date='1397/07/03' />);
+        games.push(<GameResult leftTeam='پدیده' rightTeam='پرسپولیس' leftGoals='1' rightGoals='0' date='1397/07/03' />);
 
-        let gamesFull = <GamesFull Games={games} title='بازی های رو در رو' />;
+        let gamesFull = <GamesFull Games={games} title='بازی های رو در رو' noScore="1" />;
 
         return (
             <div className="game">
                 {eventsFull}
+                <DoubleTeam Items={stats}></DoubleTeam>
                 {gamesFull}
             </div>
         );
