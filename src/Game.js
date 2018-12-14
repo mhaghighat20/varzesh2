321a3ce3a7acb37637a4d9eb504e7e2eef6de040
Mohammad Haghighat <mhaghighat@rayana.ir>
2018-12-14 22:35:36 +0330

    اصلاح صفحه بازی

--- src/Game.js
+++ src/Game.js
@@ -5,19 +5,25 @@ import { EventItem, EventsFull } from "./GameComponents/Events";
 export default class Game extends React.Component{
     render() {
         let events = [];
-        events.push(<EventItem player='شجاعی' act='گل' minute='25' />)
-        let eventsFull = <EventsFull EventItems={events} />;
+        events.push(<EventItem leftOrRight='1' player='علیپور' act='کارت زرد' minute='4' />);
+        events.push(<EventItem leftOrRight='1' player='علیپور' act='پنالتی' minute='8' />);
+        events.push(<EventItem leftOrRight='1' player='نورالهی' act='کارت زرد' minute='36' />);
+        events.push(<EventItem leftOrRight='1' player='عالیشاه -> گادوین منشا' act='تعویض' minute='67' />);
+        events.push(<EventItem leftOrRight='1' player='کامیابی نیا' act='کارت زرد' minute='73' />);
+        events.push(<EventItem leftOrRight='0' player='گرامی' act='کارت زرد' minute='75' />);
+        events.push(<EventItem leftOrRight='0' player='صدری' act='کارت زرد' minute='80' />);
+        let eventsFull = <EventsFull EventItems={events} leftTeam="پرسپولیس" rightTeam="پدیده" />;
 
 
         let games = [];
-        games.push(<GameResult leftTeam='پرسپولیس' rightTeam='ذوب آهن' leftGoals='1' rightGoals='0' date='1397/07/13' />);
-        games.push(<GameResult leftTeam='ماشین سازی تبریز' rightTeam='پرسپولیس' leftGoals='0' rightGoals='1' date='1397/07/08' />);
-        games.push(<GameResult leftTeam='پرسپولیس' rightTeam='ذوب آهن' leftGoals='1' rightGoals='0' date='1397/07/97' />);
+        games.push(<GameResult leftTeam='پرسپولیس' rightTeam='پدیده' leftGoals='1' rightGoals='0' date='1397/07/13' />);
+        games.push(<GameResult leftTeam='پدیده' rightTeam='پرسپولیس' leftGoals='0' rightGoals='1' date='1397/07/08' />);
+        games.push(<GameResult leftTeam='پدیده' rightTeam='ذوب آهن' leftGoals='1' rightGoals='0' date='1397/07/03' />);
 
         let gamesFull = <GamesFull Games={games} title='بازی های رو در رو' />;
 
         return (
-            <div>
+            <div className="game">
                 {eventsFull}
                 {gamesFull}
             </div>
