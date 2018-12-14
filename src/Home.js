83910c3b26033cf7a81ca935edbd3d9511ac7c5e
Mohammad Hossein Mostmand <mostmand.mh@gmail.com>
2018-12-14 23:23:12 +0330

    Complete homepage

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,7 @@
 import React from "react";
 import {NewsList} from "./SharedComponents/News";
 import {SportTypeEnum} from "./SharedComponents/SportType";
-import {Standings} from "./SharedComponents/Standings";
+import {GameStatusEnum, HomeUtil} from "./Utilities/HomeUtil";
 
 export default class Home extends React.Component{
     render() {
@@ -67,7 +67,7 @@ class TabPane extends React.Component{
 
         const children = <div>
             <div className="col-sm-6">
-                <Standings sportType = {this.props.sportType}/>
+                <GameList sportType = {this.props.sportType}/>
             </div>
             <div className="col-sm-6">
                 <NewsList newsIds = {['1']}/>
@@ -77,6 +77,95 @@ class TabPane extends React.Component{
         return (
             <div className="tab-pane container container-fluid" style={{display: displayType}}>
                 {children}
+            </div>
+        );
+    }
+}
+
+class GameList extends React.Component{
+    render() {
+        let leagues = HomeUtil.getNewGames(this.props.sportType);
+
+        let leagueName = leagues[0].name;
+        let weekName = leagues[0].weeks[0].name;
+
+        return (
+            <div className="panel container-fluid" style={{alignItems: "center"}}>
+                <div className="panel-heading">مسابقات</div>
+                <div className="row">
+                    <select>
+                        <option value="2">{leagueName}</option>
+                    </select>
+                </div>
+                <div className="row">
+                    <select>
+                        <option value="15">{weekName}</option>
+                    </select>
+                </div>
+                <div className="row">
+                    <WeekList week={leagues[0].weeks[0]}/>
+                </div>
+            </div>
+        );
+    }
+}
+
+class WeekList extends React.Component{
+    render() {
+        let games = [];
+        for (let i = 0; i < this.props.week.games.length; i++){
+            games.push(<GameItem game={this.props.week.games[i]}/>);
+        }
+        return (
+            <div>
+                {games}
+            </div>
+        );
+    }
+}
+
+class GameItem extends React.Component{
+    render() {
+        const game = this.props.game;
+        let gameItem;
+        if (game.status !== GameStatusEnum.notDoneYet){
+            gameItem =
+                <div className="row">
+                    <div className="col-sm-3">
+                        {game.homeTeam}
+                    </div>
+                    <div className="col-sm-1">
+                        {game.homeGoals}
+                    </div>
+                    <div className="col-sm-1">
+                        {game.awayGoals}
+                    </div>
+                    <div className="col-sm-3">
+                        {game.awayTeam}
+                    </div>
+                    <div className="col-sm-4">
+                        {game.date}
+                    </div>
+                </div>;
+        }
+        else{
+            gameItem =
+                <div className="row">
+                    <div className="col-sm-4">
+                        {game.homeTeam}
+                    </div>
+                    <div className="col-sm-4">
+                        {game.awayTeam}
+                    </div>
+                    <div className="col-sm-4">
+                        {game.date}
+                    </div>
+                </div>;
+        }
+
+        return (
+            <div className="game-item">
+                {gameItem}
             </div>
         );
     }
